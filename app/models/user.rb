33dd93184d25f30550c63b42821b1f5ae1604b68
gingin-s <ginga.capriccioso823@gmail.com>
2022-02-16 17:28:31 +0900

    エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true, message: 'is invalid. Include both letters and numbers'
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, allow_blank: true, message: "全角カタカナで入力して下さい" }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, allow_blank: true, message: "全角カタカナで入力して下さい" }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "全角カタカナで入力して下さい" }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "全角カタカナで入力して下さい" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, allow_blank: true, message: "is invalid. Input full-width characters" }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]/, allow_blank: true, message: "is invalid. Input full-width characters" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "is invalid. Input full-width katakana characters" }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "is invalid. Input full-width katakana characters" }
     validates :birthday
   end
   
