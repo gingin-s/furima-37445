747cb1636d9ee693c22998b03fa8a859f519bbd1
gingin-s <ginga.capriccioso823@gmail.com>
2022-02-17 16:34:46 +0900

    バリデーションの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,14 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :Shipping_date
-  
+
   belongs_to :user
+
+  with_options presence: true do
+  validates :title
+  validates :description
+  validates :price, nemericality: { in: 300..9999999, allow_blank: true }
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank"}
 end
