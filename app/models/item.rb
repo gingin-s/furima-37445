da43e97133f0a1dfe01476e0351c3a1f4ef45c9b
gingin-s <ginga.capriccioso823@gmail.com>
2022-02-17 17:10:46 +0900

    バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,17 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-  validates :title
-  validates :description
-  validates :image
-  validates :price, nemericality: { in: 300..9999999, allow_blank: true }
+    validates :title
+    validates :description
+    validates :image
+    validates :price, nemericality: { in: 300..9999999, allow_blank: true }
   end
 
-  with_options numericality: { other_than: 1, message: "can't be blank"}
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 end
