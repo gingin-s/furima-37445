a9a7e75f6aaea78e3b6e2511f3d10c9a969c8d55
gingin-s <ginga.capriccioso823@gmail.com>
2022-02-18 00:00:58 +0900

    new・createアクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_on_id).merge(user_id: current_user.id)
+  end
 end
