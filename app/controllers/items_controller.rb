5fdf2f1ad0e64e11114c857db4f1cae4f27cf898
gingin-s <ginga.capriccioso823@gmail.com>
2022-02-18 14:43:30 +0900

    indexアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
+    # 登録が新しい順で取得
+    @items = Item.order("created_at DESC")
   end
 
   def new
